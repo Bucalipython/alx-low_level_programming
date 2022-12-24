05b4cd4961808bc722df4f7e338178287a9c5541
Joachim <Joachimnature@gmail.com>
2022-12-24 15:39:07 -0800

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,12 +1,9 @@
 #include <stdio.h>
 
 /**
- *
- *  * main - prints the alphabet in lowercase except q and e.
- *
- *   * Return: 0
- *
- *   **/
+ * main - prints the alphabet in lowercase except q and e.
+ * Return: 0
+**/
 
 int main(void)
 {
