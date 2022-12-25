f5f218994d8a613187e1e5e2b7bf0037170314de
Joachim <Joachimnature@gmail.com>
2022-12-25 07:09:07 -0800

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,7 +6,7 @@
 
 void times_table(void)
 {
-	int i, j, op;
+	int a, j, op;
 
 	for (a = 0; a <= 9; a++)
 	{
@@ -18,7 +18,7 @@ void times_table(void)
 			_putchar(32);
 			if (op <= 9)
 			{
-				_putchar('32');
+				_putchar(32);
 				_putchar(op + 48);
 			}
 			else
